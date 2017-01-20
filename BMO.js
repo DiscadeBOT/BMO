da6166789bcb9b3533916c2dc3561e22446ec644
BarredGalaxy <DiscadeBOT@users.noreply.github.com>
2017-01-20 09:16:39 -0800

    Update BMO.js

--- BMO.js
+++ BMO.js
@@ -1,1 +1,16 @@
-
+const Discord = require('discord.js');
+const bmo = new Discord.Client();
+const prefix = "!";
+bmo.on('ready', () => {
+  console.log('I am ready!');
+});
+bmo.on('message', msg => {
+  msg.react("🤖")
+  msg.react(":regional_indicator_b:")
+  msg.react(":regional_indicator_m:")
+  msg.react(":regional_indicator_o:")
+  if (msg.content.startsWith 'bmo') {
+    msg.channel.sendMessage('I Am BMO!');
+  }
+});
+bmo.login('token');
